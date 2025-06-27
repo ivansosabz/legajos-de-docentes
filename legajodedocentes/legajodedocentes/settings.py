46e4ec02bcdf31dd00ad44cc88b2419de9c80810
ivansosabz <ivansosabz@gmail.com>
2025-06-27 19:28:38 -0300

    modificando bd y requerimientos

--- legajodedocentes/legajodedocentes/settings.py
+++ legajodedocentes/legajodedocentes/settings.py
@@ -78,17 +78,22 @@ WSGI_APPLICATION = 'legajodedocentes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "legajos_de_docentes",
+#         "USER": "postgres",
+#         "PASSWORD": "1234",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "legajos_de_docentes",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
